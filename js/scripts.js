11489510cac3e5a8873d2ab2d381407257033cb7
Christian Halsell <christianhalsell@christians-mbp.attlocal.net>
2017-08-13 16:53:12 -0700

    add modal logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
+// TODO: if input is blank, make it zero
+
 const wrapper = document.getElementById('wrapper');
+const time = document.getElementById('time');
 const display = document.getElementById('display');
 const pauseBtn = document.getElementById('pauseBtn');
 const submitBtn = document.getElementById('submitTime');
@@ -13,6 +16,18 @@ let minutes = 0;
 let seconds = 0;
 let minutesWarning = 0;
 let secondsWarning = 0;
+
+let savedMinutes = localStorage.getItem('totalMinutes');
+let savedSeconds = localStorage.getItem('totalSeconds');
+let savedWarningMinutes = localStorage.getItem('warningMinutes');
+let savedWarningSeconds = localStorage.getItem('secondsWarning');
+
+function checkSavedTime() {
+    displayTotalMinutes.value = savedMinutes ? savedMinutes : null;
+    displayTotalSeconds.value = savedSeconds ? savedSeconds : null;
+    displayWarningMinutes.value = savedWarningMinutes ? savedWarningMinutes : null;
+    displayWarningSeconds.value = savedWarningSeconds ? savedWarningSeconds : null;
+}
 
 function submitTime() {
     minutes = parseInt(displayTotalMinutes.value);
@@ -68,10 +83,23 @@ function pauseCheck() {
 
 submitBtn.addEventListener('click', function () {
     submitTime();
-    displayTime();
-    modal.classList.add('hide');
+
+    if (isNaN(minutes) || isNaN(seconds) || isNaN(minutesWarning) || isNaN(secondsWarning)) {
+        alert('You must enter numbers for time, silly!');
+    } else {
+
+        // Add to local storage
+        localStorage.setItem('totalMinutes', minutes);
+        localStorage.setItem('totalSeconds', seconds);
+        localStorage.setItem('warningMinutes', minutesWarning);
+        localStorage.setItem('secondsWarning', secondsWarning);
+
+        time.classList.remove('hide');
+        displayTime();
+        modal.classList.add('hide');
+    }
 })
 pauseBtn.addEventListener('click', pauseCheck);
 
-// displayTime();
+checkSavedTime();
 setInterval(countdown, 1000);
